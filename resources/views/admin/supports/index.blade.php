63bee2d804e64409bb99f88b7d37661e0d9d2352
chtwikee <carlosh.sales@outlook.com>
2023-07-14 17:21:43 -0300

    fix: object to array in view

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,6 +1,6 @@
 <h1>Support list</h1>
 
-<a href="{{route('supports.create')}}">add new support</a>
+<a href="{{ route('supports.create') }}">add new support</a>
 
 <table>
     <thead>
@@ -10,13 +10,13 @@
     </thead>
 
     <tbody>
-        @foreach($supports as $support)
+        @foreach ($supports as $support)
             <tr>
-                <td>{{$support->subject}}</td>
-                <td>{{$support->status}}</td>
-                <td>{{$support->body}}</td>
-                <td><a href="{{ route('supports.show', $support->id) }}">></a></td>
-                <td><a href="{{ route('supports.edit', $support->id) }}">edit</a></td>
+                <td>{{ $support['subject'] }}</td>
+                <td>{{ $support['status'] }}</td>
+                <td>{{ $support['body'] }}</td>
+                <td><a href="{{ route('supports.show', $support['id']) }}">></a></td>
+                <td><a href="{{ route('supports.edit', $support['id']) }}">edit</a></td>
             </tr>
         @endforeach
     </tbody>
